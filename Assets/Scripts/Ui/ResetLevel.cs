b23512b5f7ba26c52f52b6800e6dc97b57f6998c
BlazingTwist <blazingtwist1@gmail.com>
2023-07-16 16:33:25 +0200

    schilder in allen levels unterstützen, reset level button

---
+++ Assets/Scripts/Ui/ResetLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Ui {
+
+	public class ResetLevel : MonoBehaviour {
+		public void Execute() {
+			GameManager.GetInstance().UnPause();
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+	}
+
+}
