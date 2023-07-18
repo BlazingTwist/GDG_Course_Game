00688a898e39e0cefb9475138369ce70172ecbbc
Unknown <andre.li@t-online.de>
2023-07-18 03:09:26 +0200

    Added itemmanger und collecibleitem palzieren der 4 items

---
+++ Assets/Scripts/ItemManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using World;
+using UnityEngine.SceneManagement;
+
+public class ItemManager : MonoBehaviour
+{
+    private static ItemManager instance;
+    public GameObject axe;
+    public GameObject envelope;
+    public GameObject map;
+    public GameObject rope;
+
+    private bool axeCollected;
+    private bool envelopeCollected = false;
+    private bool mapCollected = false;
+    private bool ropeCollected = false;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+        UpdateItems();
+    }
+
+    public static ItemManager GetInstance()
+    {
+        return instance;
+    }
+
+    public void UpdateItems()
+    {
+        axe.SetActive(axeCollected);
+        envelope.SetActive(envelopeCollected);
+        map.SetActive(mapCollected);
+        rope.SetActive(ropeCollected);
+    }
+
+    public void CollectAxe()
+    {
+        axeCollected = true;
+        UpdateItems();
+    }
+
+    public void CollectEnvelope()
+    {
+        envelopeCollected = true;
+        UpdateItems();
+    }
+
+    public void CollectMap()
+    {
+        mapCollected = true;
+        UpdateItems();
+    }
+
+    public void CollectRope()
+    {
+        ropeCollected = true;
+        UpdateItems();
+    }
+
+
+}
+
---
+++ Assets/Scripts/World/CollectibleItem.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace World
+{
+
+    public class CollectibleItem : Interactable
+    {
+        protected ItemManager itemManager;
+        public string itemName;
+        public override void OnInteractComplete()
+        {
+            base.OnInteractComplete();
+
+            if (!gameObject.activeSelf)
+            {
+                return;
+            }
+
+            levelProgressManager.OnCollectibleGotten();
+            gameObject.SetActive(false);
+
+            itemManager = ItemManager.GetInstance();
+            ActivateItemInUI(itemName);
+
+
+        }
+
+        public void ActivateItemInUI(string name)
+        {
+            if (name == "Axe")
+            {
+                itemManager.CollectAxe();
+            }
+            if (name == "Envelope")
+            {
+                itemManager.CollectEnvelope();
+            }
+            if (name == "Map")
+            {
+                itemManager.CollectMap();
+            }
+            if (name == "Rope")
+            {
+                itemManager.CollectRope();
+            }
+
+        }
+    }
+
+}
